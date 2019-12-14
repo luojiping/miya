baab3bb37256f5ef8e86259d2d3c439abffcd469
luojiping <18893782895@163.com>
2019-12-14 08:59:51 +0800

    ljp提交

--- src/pages/home/components/content/rob.js
+++ src/pages/home/components/content/rob.js
@@ -1,56 +1,53 @@
 import React, { Component, useImperativeHandle } from 'react'
 // 引入axios，请求数据
 import axios from "axios"
+import All from "./all.js"
+import List from "./list.js"
+import Year from "./year.js"
 import "../../../../assets/css/home/rob.css"
 class Rob extends Component {
     state = {
-        data: []
+        data: [],
+        goods: {
+            name:"年年有货",
+            xiaoxi:"麻辣零食大礼包28.9元抢",
+            background:1
+        },
+        goods2: {
+            name:"家居百货",
+            xiaoxi:"手帕纸30包9.9元抢",
+            background:2
+        }
     }
     render() {
         return (
             <>
                 <div className="todaylist">
-                    <div className="todaylist_tit">今日必抢<span>·好团价</span></div>
-                    <ul className="todaylist_content">
-                        {
-                            this.state.data[0]&&this.state.data[0].map((item) => {
-                                return (
-                                    <li key={item.sku}>
-                                        <img src={item.picture}></img>
-                                        <div className="price">
-                                            <span>￥{item.groupon_price}</span>
-                                            <i>￥{item.market_price}</i>
-                                        </div>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                    <div className="list">
-                        {
-                            this.state.data[0]&&this.state.data[1].map((item) => {
-                                return (
-                                    <div className="lists" key={item.sku}>
-                                        <img src={item.picture}></img>
-                                        <div className="details">
-                                            <div>
-                                                <div className="title">{item.name}</div>
-                                                <div className="describe">{item.intro}</div>
-                                            </div>
+                    <div className="todaylist-top">
+                        <div className="todaylist_tit">今日必抢<span>·好团价</span></div>
+                        <ul className="todaylist_content">
+                            {
+                                this.state.data[0] && this.state.data[0].map((item) => {
+                                    return (
+                                        <li key={item.sku}>
+                                            <img src={item.picture}></img>
                                             <div className="price">
-                                                <div className="price_left">
-                                                    <p className="unit_price">单买价￥{item.sale_price}</p>
-                                                    <p className="sale">￥{item.groupon_price}</p>
-                                                    <p className="sprice">已拼{item.sale_count}件</p>
-                                                </div>
-                                                <div className="price_right">去拼团</div>
+                                                <span>￥{item.groupon_price}</span>
+                                                <i>￥{item.market_price}</i>
                                             </div>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
                     </div>
+                    <List list={this.state.data[1]} />
+                    <All goods={this.state.goods} />
+                    <Year year={this.state.data[2]} />
+                    <List list={this.state.data[3]} />
+                    <All goods={this.state.goods2} />
+                    <Year year={this.state.data[4]} />
+                    <List list={this.state.data[5]} />
                 </div>
             </>
         )
@@ -58,34 +55,37 @@ class Rob extends Component {
     getList() {
         axios.get("/list")
             .then(res => {
-                console.log(res)
-                console.log(res.data.data_list)
+                // console.log(res)
+                // console.log(res.data.data_list)
                 var arr = [];
                 var arr2 = []
                 var arr3 = []
                 var arr4 = []
                 var arr5 = []
-                
-                for( let i=0;i<res.data.data_list.length;i++){
-                    if(i<3){
-                        arr.push(res.data.data_list[i])    
-                    }else if(i<7){
-                        arr2.push(res.data.data_list[i]) 
-                    }else if(i<12){
+                var arr6 = []
+
+                for (let i = 0; i < res.data.data_list.length; i++) {
+                    if (i < 3) {
+                        arr.push(res.data.data_list[i])
+                    } else if (i < 7) {
+                        arr2.push(res.data.data_list[i])
+                    } else if (i < 11) {
                         arr3.push(res.data.data_list[i])
-                    }else if(i<17){
+                    } else if (i < 15) {
                         arr4.push(res.data.data_list[i])
-                    }else{
+                    } else if (i < 20) {
                         arr5.push(res.data.data_list[i])
                     }
+                    else {
+                        arr6.push(res.data.data_list[i])
+                    }
                 }
-                const arrAll = [arr,arr2,arr3,arr4,arr5] 
+                const arrAll = [arr, arr2, arr3, arr4, arr5, arr6]
                 this.setState({
-                  data: arrAll
+                    data: arrAll
                 })
 
                 console.log(this.state.data)
-                console.log(this.state.data[0])
             })
     }
     // 获取axios
--- src/pages/home/components/header/tabbar.js
+++ src/pages/home/components/header/tabbar.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
 import { Tabs } from 'antd-mobile';
 import Conent from "../content/conent.js"
+import Lists from "../content/lists.js"
 import "../../../../assets/css/home/tabbar.css";
 
 class TabBar extends Component {
-    renderContent = tab =>
-        (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-            {/* <p>Content of {tab.title}</p> */}
-        </div>
-        );
+    state = {
+        tabs: [
+            { title: '首页', id: "0" },
+            { title: '母婴', id: "21" },
+            { title: '玩具', id: "22" },
+            { title: '孕产', id: "23" },
+            { title: '美妆', id: "24" },
+            { title: '美食', id: "25" },
+            { title: '童装', id: "26" },
+            { title: '百货', id: "27" },
+            { title: '女装', id: "28" },
+            { title: '生鲜', id: "29" },
+        ]
+    }
+    // renderContent = tab =>
+    //     (
+    //         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+    //             {/* {
+    //                 this.state.tabs.map((item, index) => {
+    //                     return (
+    //                         // console.log(item.id)
+    //                         item.id === 0 ? (<Conent />) : (<Lists tabId={item.id} />)
+    //                     )
+    //                 })
+    //             } */}
+    //         </div>
+    //     );
     render() {
-        const tabs = [
-            { title: '首页' },
-            { title: '母婴' },
-            { title: '玩具' },
-            { title: '孕产' },
-            { title: '美妆' },
-            { title: '美食' },
-            { title: '童装' },
-            { title: '百货' },
-            { title: '女装' },
-            { title: '生鲜' },
-        ];
         return (
-            <div>
-                <Tabs tabs={tabs} renderTabBar={props =>
-                    <Tabs.DefaultTabBar {...props} page={7}/>}>
-                    {this.renderContent}
+            <div className="tabbar">
+                <Tabs tabs={this.state.tabs} renderTabBar={props =>
+                    <Tabs.DefaultTabBar
+                        {...props} page={7}
+                    />}>
+                    <Conent />
+                    <Lists listId={this.state.tabs[0].id} />
+                    <Lists listId={this.state.tabs[1].id}/>
+                    <Lists listId={this.state.tabs[2].id}/>
+                    <Lists listId={this.state.tabs[3].id}/>
+                    <Lists listId={this.state.tabs[4].id}/>
                 </Tabs>
-                <Conent/>
+
             </div>
         )
     }
